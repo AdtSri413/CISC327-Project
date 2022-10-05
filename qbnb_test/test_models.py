253ebd92a8c5b708811b4fab2aeeecc9f6d49a1a
AdtSri413 <aditi.srinivasan@gmail.com>
2022-10-04 23:01:24 -0400

    I give up. Maybe it'll work once we inplement actual tests

--- qbnb_test/test_models.py
+++ qbnb_test/test_models.py
@@ -1,9 +1,3 @@
 '''
 Test code for qbnb/models.py (Assignment 1 models and assignment 2 functions)
 '''
-
-from qbnb.models import *
-
-def app_context():
-    with app.app_context():
-        yield
