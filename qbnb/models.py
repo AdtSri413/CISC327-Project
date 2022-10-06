8b50c10f8fb66a8889d4f6664317e360c94b4d64
Malthus Oh <malthusoh@gmail.com>
2022-10-06 15:58:15 -0600

    updated user model with required fields i.e. billing addresss and postal code

--- qbnb/models.py
+++ qbnb/models.py
@@ -14,20 +14,32 @@ class User(db.Model):
     '''
     # Unique ID of the user
     # -> primary key of the model
-    user_id = db.Column(db.Integer, primary_key=True)
-    # First Name of the user
-    user_first_name = db.Column(db.String(120), unique=False, nullable=False)
-    # Last Name of the user
-    user_last_name = db.Column(db.String(120), unique=False, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    
     # Email Address of the user
     # -> must be unique since one email can only open one account
-    user_email = db.Column(db.String(120), unique=True, nullable=False)
-    # Username of the user's account -> must be unique to identify each account
-    user_username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
     # Password of the user's account -> doesn't have to be unique
-    user_password = db.Column(db.String(120), unique=False, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+    
+    # Username of the user's account -> must be unique to identify each account
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    
+    # Billing Address
+    billing_address = db.Column(db.String(120), unique=False, nullable=False)
+
+    # Postal Code
+    postal_code = db.Column(db.String(120), unique=False, nullable=False)
+    
     # Account Balance
-    user_account_balance = db.Column(db.Float, unique=False, nullable=False)
+    balance = db.Column(db.Float, unique=False, nullable=False)
+    
+    # First Name of the user
+    first_name = db.Column(db.String(120), unique=False, nullable=False)
+    
+    # Last Name of the user
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
     
     def __repr__(self):
         return '<User %r>' % self.username
