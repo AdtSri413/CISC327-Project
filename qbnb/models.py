15e28719d9fbb8d5e87f11292a647b59405c400d
Yap Yu Xiang <shawn@Yaps-MacBook-Pro-2.local>
2022-10-10 19:44:42 -0700

    Create login function

--- qbnb/models.py
+++ qbnb/models.py
@@ -1,5 +1,6 @@
 from qbnb import app
 from flask_sqlalchemy import SQLAlchemy
+from email_validator import validate_email, EmailNotValidError
 
 '''
 This file defines data models and related business logics
@@ -14,20 +15,20 @@ class User(db.Model):
     '''
     # Unique ID of the user
     # -> primary key of the model
-    user_id = db.Column(db.Integer, primary_key=True)
-    # First Name of the user
-    user_first_name = db.Column(db.String(120), unique=False, nullable=False)
-    # Last Name of the user
-    user_last_name = db.Column(db.String(120), unique=False, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     # Email Address of the user
     # -> must be unique since one email can only open one account
-    user_email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     # Username of the user's account -> must be unique to identify each account
-    user_username = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     # Password of the user's account -> doesn't have to be unique
-    user_password = db.Column(db.String(120), unique=False, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+    # Billing Address
+    billing_address = db.Column(db.String(120), unique=False, nullable=False)
+    # Postal Code
+    postal_code = db.Column(db.String(120), unique=False, nullable=False)
     # Account Balance
-    user_account_balance = db.Column(db.Float, unique=False, nullable=False)
+    balance = db.Column(db.Float, unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -117,3 +118,165 @@ db.create_all()
 
 
 # Put Assignment 2 functions here
+
+def verify_email(email):
+    '''
+    Helper function to validate email
+        Parameters:
+            email (string): user email
+        Returns:
+            True if email is valid, otherwise False
+    '''
+    # R1-1: Cannot be empty
+    if email is None or email == "":
+        return False
+
+    # R1-3: Must follow RFC 5322 specifications
+    try:
+        validate_email(email, check_deliverability=False)
+    except EmailNotValidError as e:
+        print(str(e))
+        return False
+
+    # R1-7: Cannot be duplicate
+    valids = User.query.filter_by(email=email).all()
+    if len(valids) == 1:
+        return False
+    return True
+
+
+def verify_password(password):
+    '''
+    Helper function to validate password
+        Parameters:
+            password (string): user password
+        Returns:
+            True if password is valid, otherwise False
+    '''
+    # R1-1: Cannot be empty.
+    if password is None or password == "":
+        return False
+
+    # R1-4: Minimum length 6
+    if len(password) <= 6:
+        return False
+
+    # R1-4: At least one special character
+    special_characters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    if not any(c in special_characters for c in password):
+        return False
+
+    # R1-4: At least one lower case
+    if not any(c.islower() for c in password):
+        return False
+
+    # R1-4: At least one upper case
+    if not any(c.isupper() for c in password):
+        return False
+
+    print("verify password returns True")
+
+    return True
+
+
+def verify_username(username):
+    '''
+    Helper function to validate username
+        Parameters:
+            username (string): user username
+        Returns:
+            True if username is valid, otherwise False
+    '''
+    # R1-5: Cannot be empty
+    if username is None or username == "":
+        return False
+
+    # R1-5: Alphanumeric only
+    if not username.replace(" ", "").isalnum():
+        return False
+
+    # R1-5: Space allowed only if not prefix or suffix
+    if username[0] == " " or username[-1] == " ":
+        return False
+
+    # R1-6: Longer than 2 characters and less than 20 characters
+    if len(username) <= 2 or len(username) >= 20:
+        return False
+
+    return True
+
+
+def verify_email_login(email):
+    '''
+    Helper function to validate email
+        Parameters:
+            email (string): user email
+        Returns:
+            True if email is valid, otherwise False
+    '''
+    # R1-1: Cannot be empty
+    if email is None or email == "":
+        return False
+
+    # R1-3: Must follow RFC 5322 specifications
+    try:
+        validate_email(email, check_deliverability=False)
+    except EmailNotValidError as e:
+        print(str(e))
+        return False
+
+    # Email must exist in database
+    valids = User.query.filter_by(email=email).all()
+    if len(valids) != 1:
+        return False
+    return True
+
+
+def register(username, email, password):
+    '''
+    Check register parameters
+        Parameters:
+            username (string): user username
+            email (string): user email
+            password (string): user password
+        Returns:
+            User object if register succeeds, otherwise None
+    '''
+    if not verify_username(username):
+        return None
+    if not verify_email(email):
+        return None
+    if not verify_password(password):
+        return None
+    # R1-8: Billing address is empty at registration
+    # R1-9: Postal code is empty at registration
+    # R1-10: Balance initialised as 100 at registration
+    new_user = User(username=username, email=email, password=password,
+                    billing_address="", postal_code="", balance=100)
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user
+
+
+def login(email, password):
+    '''
+    Check login parameters
+        Parameters:
+            email (string):    user email
+            password (string): user password
+        Returns:
+            The user object if login succeeded otherwise None
+    '''
+
+    # Email and password should meet the required specification
+    if not verify_email_login(email):
+        return None
+
+    if not verify_password(password):
+        return None
+
+    # Check database to see to see if email and password match
+    valids = User.query.filter_by(email=email, password=password).all()
+    if len(valids) != 1:
+        return None
+    return valids[0]
--- qbnb_test/test_models.py
+++ qbnb_test/test_models.py
@@ -1,3 +1,63 @@
 '''
 Test code for qbnb/models.py (Assignment 1 models and assignment 2 functions)
 '''
+
+from qbnb.models import register, login
+
+
+def test_r2_1_login():
+    '''
+    Testing R2-1: A user can log in using her/his email address 
+      and the password.
+      (will be tested after the previous test, so we already have u0, 
+      in database)
+    '''
+
+    register(username="user 0", email="test0@test.com", password="Test123?")
+
+    user = login('test0@test.com', 'Test123?')
+    assert user is not None
+    assert user.username == 'user 0'
+
+    # Incorrect password
+    user = login('test0@test.com', '1234567')
+    assert user is None
+
+    # Incorrect email
+    user = login('test1@test.com', '123456')
+    assert user is None
+
+
+def test_r2_2_login():
+    '''
+    Testing R2-2: The login function should check if the supplied inputs 
+    meet the same email/password requirements as above, before checking the database.
+    (will be tested after the previous test, so we already have u0, 
+      u1 in database)
+    '''
+
+    # Email should not be empty
+    user = login("", "Test123?")
+    assert user is None
+    user = login(None, "Test123?")
+    assert user is None
+
+    # Email has to follow addr-spec defined in RFC 5322
+    user = login("test1.@test.com", "Test123?")
+    assert user is None
+
+    # Password does not meet minimum length of 6
+    user = login('test0@test.com', 'Ab@12')
+    assert user is None
+
+    # Password does not have at least 1 upper case
+    user = login('test0@test.com', 'ab@123')
+    assert user is None
+
+    # Password does not have at least 1 lower case
+    user = login('test0@test.com', 'AB@123')
+    assert user is None
+
+    # Password does not have at least 1 special case
+    user = login('test0@test.com', 'Abc123')
+    assert user is None
