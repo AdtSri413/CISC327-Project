19c24ad46c5dfa2209d9efc97f5dcb8ffccb7f19
AdtSri413 <aditi.srinivasan@gmail.com>
2022-10-11 15:47:06 -0400

    TEMPLATE: Trying to fix the pytest error found on github by introducing a temporaty test

--- qbnb_test/test_models.py
+++ qbnb_test/test_models.py
@@ -1,3 +1,10 @@
 '''
 Test code for qbnb/models.py (Assignment 1 models and assignment 2 functions)
 '''
+
+import qbnb.models
+
+
+def ex_test():
+    assert (2 == 2) is True
+    assert (3 == 4) is False
