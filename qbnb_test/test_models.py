43c688c5bb3f45525ea55592f455df684d18fc48
Yap Yu Xiang <shawn@Yaps-MacBook-Pro-2.local>
2022-10-11 21:29:25 -0700

    Modified test_model code

--- qbnb_test/test_models.py
+++ qbnb_test/test_models.py
@@ -113,139 +113,6 @@ def test_r1_10_register():
     assert user.balance == 100
 
 
-def test_r2_1_login():
-    '''
-    Testing R2-1: A user can log in using her/his email address 
-      and the password.
-      (will be tested after the previous test, so we already have u0, 
-      in database)
-    '''
-
-    register(username="user 0", email="test0@test.com", password="Test123?")
-
-    user = login('test0@test.com', 'Test123?')
-    assert user is not None
-    assert user.username == 'user 0'
-
-    # Incorrect password
-    user = login('test0@test.com', '1234567')
-    assert user is None
-
-    # Incorrect email
-    user = login('test1@test.com', '123456')
-    assert user is None
-
-
-def test_r2_2_login():
-    '''
-    Testing R2-2: The login function should check if the supplied inputs 
-    meet the same email/password requirements as above, before checking 
-    the database.
-    (will be tested after the previous test, so we already have u0, 
-      u1 in database)
-    '''
-
-    # Email should not be empty
-    user = login("", "Test123?")
-    assert user is None
-    user = login(None, "Test123?")
-    assert user is None
-
-    # Email has to follow addr-spec defined in RFC 5322
-    user = login("test1.@test.com", "Test123?")
-    assert user is None
-
-    # Password does not meet minimum length of 6
-    user = login('test0@test.com', 'Ab@12')
-    assert user is None
-
-    # Password does not have at least 1 upper case
-    user = login('test0@test.com', 'ab@123')
-    assert user is None
-
-    # Password does not have at least 1 lower case
-    user = login('test0@test.com', 'AB@123')
-    assert user is None
-
-    # Password does not have at least 1 special case
-    user = login('test0@test.com', 'Abc123')
-    assert user is None
-
-
-def test_r3_1_update_user():
-    '''
-    Testing R3-1: The update_user function is able to update user's 
-    user name, user email, billing address and postal code
-    '''
-    user = update_user(user_id=1, username="Tommy", email="tommy@gmail.com",
-                       billing_address="45 Union Street", postal_code="K7L2N8")
-    assert user is not None
-    assert user.username == "Tommy"
-    assert user.email == "tommy@gmail.com"
-    assert user.billing_address == "45 Union Street"
-    assert user.postal_code == "K7L2N8"
-
-
-def test_r3_2_update_user():
-    '''
-    Testing R3-2: The update_user function should check if the 
-    supplied postal code non-empty, alphanumeric-only and has 
-    no special characters, before checking the database.
-    '''
-    # Supplied postal code should be non-empty
-    user = update_user(user_id=1, postal_code="")
-    assert user is None
-
-    # Supplied postal code should be alphanumeric only
-    user = update_user(user_id=1, postal_code="K7L 2N8")
-    assert user is None
-
-    # Supplied postal code sould have no special characters
-    user = update_user(user_id=1, postal_code="K7L2N@")
-    assert user is None
-
-
-def test_r3_3_update_user():
-    '''
-    Testing R3-3: The update_user function should check if the supplied postal 
-    code is a valid canadian postal code, before checking the database
-    '''
-
-    # Supplied postal code should be a valid canadian postal code
-    # (leading Z is not allowed)
-    user = update_user(user_id=1, postal_code="Z7L2N8")
-    assert user is None
-
-
-def test_r3_4_update_user():
-    '''
-    Testing R3-4: The update_user function should check if the supplied 
-    username follows the above requirement, before checking the database
-
-    '''
-
-    # Supplied username should be non-empty
-    user = update_user(user_id=1, postal_code="")
-    assert user is None
-
-    # Supplied username should be alphanumeric-only
-    user = update_user(user_id=1, postal_code="K7L 2N8")
-    assert user is None
-
-    # Supplied username should have space allowed only if it is not
-    # as the prefix or suffix
-    user = update_user(user_id=1, username=" K7L2N8")
-    assert user is None
-
-    # Supplied username should be longer than 2 characters
-    user = update_user(user_id=1, username="a")
-    assert user is None
-
-    # Supplied username should be less than 20 characters
-    user = update_user(user_id=1, username="abcdefghijklmnopqrstu")
-    assert user is None
-
-
 def test_r4_1_create_listing():
     '''
     Testing R4-1: If the title is not alphanumeric only or begins/ends
@@ -396,3 +263,136 @@ def test_r4_8_create_listing():
     assert create_listing(
         'Ex title R4 8', 'Ex description of listing',
         1000, '2022-10-05', 'test0@test.com') is False
+
+
+def test_r2_1_login():
+    '''
+    Testing R2-1: A user can log in using her/his email address 
+      and the password.
+      (will be tested after the previous test, so we already have u0, 
+      in database)
+    '''
+
+    register(username="user 0", email="test0@test.com", password="Test123?")
+
+    user = login('test0@test.com', 'Test123?')
+    assert user is not None
+    assert user.username == 'user 0'
+
+    # Incorrect password
+    user = login('test0@test.com', '1234567')
+    assert user is None
+
+    # Incorrect email
+    user = login('test1@test.com', '123456')
+    assert user is None
+
+
+def test_r2_2_login():
+    '''
+    Testing R2-2: The login function should check if the supplied inputs 
+    meet the same email/password requirements as above, before checking 
+    the database.
+    (will be tested after the previous test, so we already have u0, 
+      u1 in database)
+    '''
+
+    # Email should not be empty
+    user = login("", "Test123?")
+    assert user is None
+    user = login(None, "Test123?")
+    assert user is None
+
+    # Email has to follow addr-spec defined in RFC 5322
+    user = login("test1.@test.com", "Test123?")
+    assert user is None
+
+    # Password does not meet minimum length of 6
+    user = login('test0@test.com', 'Ab@12')
+    assert user is None
+
+    # Password does not have at least 1 upper case
+    user = login('test0@test.com', 'ab@123')
+    assert user is None
+
+    # Password does not have at least 1 lower case
+    user = login('test0@test.com', 'AB@123')
+    assert user is None
+
+    # Password does not have at least 1 special case
+    user = login('test0@test.com', 'Abc123')
+    assert user is None
+
+
+def test_r3_1_update_user():
+    '''
+    Testing R3-1: The update_user function is able to update user's 
+    user name, user email, billing address and postal code
+    '''
+    user = update_user(user_id=1, username="Tommy", email="tommy@gmail.com",
+                       billing_address="45 Union Street", postal_code="K7L2N8")
+    assert user is not None
+    assert user.username == "Tommy"
+    assert user.email == "tommy@gmail.com"
+    assert user.billing_address == "45 Union Street"
+    assert user.postal_code == "K7L2N8"
+
+
+def test_r3_2_update_user():
+    '''
+    Testing R3-2: The update_user function should check if the 
+    supplied postal code non-empty, alphanumeric-only and has 
+    no special characters, before checking the database.
+    '''
+    # Supplied postal code should be non-empty
+    user = update_user(user_id=1, postal_code="")
+    assert user is None
+
+    # Supplied postal code should be alphanumeric only
+    user = update_user(user_id=1, postal_code="K7L 2N8")
+    assert user is None
+
+    # Supplied postal code sould have no special characters
+    user = update_user(user_id=1, postal_code="K7L2N@")
+    assert user is None
+
+
+def test_r3_3_update_user():
+    '''
+    Testing R3-3: The update_user function should check if the supplied postal 
+    code is a valid canadian postal code, before checking the database
+    '''
+
+    # Supplied postal code should be a valid canadian postal code
+    # (leading Z is not allowed)
+    user = update_user(user_id=1, postal_code="Z7L2N8")
+    assert user is None
+
+
+def test_r3_4_update_user():
+    '''
+    Testing R3-4: The update_user function should check if the supplied 
+    username follows the above requirement, before checking the database
+
+    '''
+
+    # Supplied username should be non-empty
+    user = update_user(user_id=1, postal_code="")
+    assert user is None
+
+    # Supplied username should be alphanumeric-only
+    user = update_user(user_id=1, postal_code="K7L 2N8")
+    assert user is None
+
+    # Supplied username should have space allowed only if it is not
+    # as the prefix or suffix
+    user = update_user(user_id=1, username=" K7L2N8")
+    assert user is None
+
+    # Supplied username should be longer than 2 characters
+    user = update_user(user_id=1, username="a")
+    assert user is None
+
+    # Supplied username should be less than 20 characters
+    user = update_user(user_id=1, username="abcdefghijklmnopqrstu")
+    assert user is None
