94b2dc9030be1757706eec1e42d77178e29b360a
Malthus Oh <malthusoh@gmail.com>
2022-10-11 21:19:50 -0400

    removed first and last name fields since not required

--- qbnb/models.py
+++ qbnb/models.py
@@ -35,12 +35,6 @@ class User(db.Model):
     # Account Balance
     balance = db.Column(db.Float, unique=False, nullable=False)
     
-    # First Name of the user
-    first_name = db.Column(db.String(120), unique=False, nullable=False)
-    
-    # Last Name of the user
-    last_name = db.Column(db.String(120), unique=False, nullable=False)
-    
     def __repr__(self):
         return '<User %r>' % self.username
 
