b47d14a96ffc5ce56775bbf6ab18884d4f3766f0
AdtSri413 <aditi.srinivasan@gmail.com>
2022-11-11 17:30:47 -0500

    Fixed pep8 errors

--- qbnb_test/frontend/test_create_listing.py
+++ qbnb_test/frontend/test_create_listing.py
@@ -22,7 +22,8 @@ class FrontEndCreateListingTest(BaseCase):
       - P4: longer than 80 characters
 
     Partitions for the Description Box:
-      - P5: Description is between 20 and 2000 characters long, is longer than the title
+      - P5: Description is between 20 and 2000 characters long, 
+            is longer than the title
       - P6: Description is missing
       - P7: Description is less than 20 characters long
       - P8: Description is more than 2000 characters long
@@ -35,23 +36,28 @@ class FrontEndCreateListingTest(BaseCase):
       - P13: Price is more than 10000
     
 
-    T2: Utilises OUTPUT PARTITION TESTING to test all possible output partitions
+    T2: Utilises OUTPUT PARTITION TESTING to test all possible output 
+        partitions
     
     Output partitions:
-      - P1: Listing is created. Website sends user back to homepage and displays a new listing
+      - P1: Listing is created. Website sends user back to homepage 
+            and displays a new listing
 
-        NOTE: If exhaustive output coverage testing were used, we would have to test for all 
-              the possible listings that could be created, which is not feasible.
+        NOTE: If exhaustive output coverage testing were used, we 
+              would have to test for all the possible listings that 
+              could be created, which is not feasible.
       
       - P2: Listing cannot be created because of invalid inputs. 
-        Website keeps user on the create listing page and displays an error message.
+        Website keeps user on the create listing page and displays an 
+        error message.
       
-      - P3: There is an invalid input parameter, but the website catches it before attempting 
-        to create a listing. Website keeps user on the create listing page, but does not 
-        display error message.
+      - P3: There is an invalid input parameter, but the website 
+            catches it before attempting to create a listing. Website 
+            keeps user on the create listing page, but does not display 
+            error message.
 
-        NOTE: A warning flag is shown in this case, but it is not an element on the website,
-              so it's presence cannot be tested for. 
+        NOTE: A warning flag is shown in this case, but it is not an 
+              element on the website, so it's presence cannot be tested for. 
     
     
     T3: Utilises FUNCTIONALITY COVERAGE TESTING to test the system's actions
@@ -90,7 +96,6 @@ class FrontEndCreateListingTest(BaseCase):
       - P2: User creates 2 listings with the same name
     '''
 
-
     # Helper function to register a new user and log that user in.
     # This needs to be done before a listing can be created
     def create_user_and_login(self, name, email, password):
@@ -107,8 +112,8 @@ class FrontEndCreateListingTest(BaseCase):
         # submit registration form
         self.click('input[type="submit"]')
 
-
-    # T1-P1, T1-P5, T1-P10, T2-P1, T3-R1-P1, T3-R2-P1, T3-R3-P2, T3-R4-P1, T3-R5-P2
+    # T1-P1, T1-P5, T1-P10, T2-P1, T3-R1-P1, T3-R2-P1, T3-R3-P2, 
+    # T3-R4-P1, T3-R5-P2
     def test_title_correct(self, *_):
         self.create_user_and_login("Example0", "example0@gmail.com", "Examp!3")
 
@@ -128,7 +133,6 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_element("#welcome-header")
         self.assert_element("#listings")
         self.assert_text("A Title", "#title")
-    
 
     # T1-P2, T2-P2, T3-R1-P2
     def test_not_alphanumeric(self, *_):
@@ -148,7 +152,6 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_element("#message")
         self.assert_text("Could not create listing.", "#message")
 
-
     # T1-P3, T2-P3
     def test_no_title(self, *_):
         self.create_user_and_login("Example2", "example2@gmail.com", "Examp!3")
@@ -167,7 +170,6 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_text("Create a New Listing!", "#header")
         self.assert_element_not_visible("#message")
 
-
     # T1-P4, T3-R2-P2
     def test_long_title(self, *_):
         self.create_user_and_login("Example3", "example3@gmail.com", "Examp!3")
@@ -176,9 +178,9 @@ class FrontEndCreateListingTest(BaseCase):
         self.open(base_url + '/create_listing')
 
         # long title, valid description and price
-        title = "A long title"*10
+        title = "A long title" * 10
         self.type("#title", title)
-        self.type("#description", "A description for the listing"*10)
+        self.type("#description", "A description for the listing" * 10)
         self.type("#price", 100)
 
         # submit create_listing form
@@ -188,7 +190,6 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_element("#welcome-header")
         self.assert_element("#listings")
         self.assert_text(title[:80], "#title")
-
 
     # T1-P6
     def test_no_description(self, *_):
@@ -207,7 +208,6 @@ class FrontEndCreateListingTest(BaseCase):
         # Should still be on create_listing page with no message appearing
         self.assert_text("Create a New Listing!", "#header")
         self.assert_element_not_visible("#message")
-
     
     # T1-P7, T3-R3-P1
     def test_short_description(self, *_):
@@ -228,7 +228,6 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_text("Create a New Listing!", "#header")
         self.assert_element_not_visible("#message")
 
-
     # T1-P8, T3-R3-P3
     def test_long_description(self, *_):
         self.create_user_and_login("Example6", "example6@gmail.com", "Examp!3")
@@ -237,7 +236,7 @@ class FrontEndCreateListingTest(BaseCase):
         self.open(base_url + '/create_listing')
 
         # valid title, long description, valid price
-        description = "Description"*200
+        description = "Description" * 200
         self.type("#title", "A title")
         self.type("#description", description)
         self.type("#price", 100)
@@ -250,7 +249,6 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_element("#listings")
         self.assert_text(description[:2000], "#description")
 
-
     # T1-P9, T3-R4-P2
     def test_description_shorter_than_title(self, *_):
         self.create_user_and_login("Example7", "example7@gmail.com", "Examp!3")
@@ -259,7 +257,8 @@ class FrontEndCreateListingTest(BaseCase):
         self.open(base_url + '/create_listing')
 
         # valid title. Description shorter than title. Valid price
-        self.type("#title", "A Long Listing Title That is Longer Than the Description")
+        self.type("#title", "A Long Listing Title That is Longer Than the \
+            Description")
         self.type("#description", "A description for this listing")
         self.type("#price", 100)
 
@@ -268,7 +267,6 @@ class FrontEndCreateListingTest(BaseCase):
 
         self.assert_element("#message")
         self.assert_text("Could not create listing.", "#message")
-
     
     # T1-P11
     def test_no_price(self, *_):
@@ -288,7 +286,6 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_text("Create a New Listing!", "#header")
         self.assert_element_not_visible("#message")
 
-    
     # T1-P12, T3-R5-P1
     def test_low_price(self, *_):
         self.create_user_and_login("Example9", "example9@gmail.com", "Examp!3")
@@ -307,11 +304,11 @@ class FrontEndCreateListingTest(BaseCase):
         # Should still be on create_listing page with no message appearing
         self.assert_text("Create a New Listing!", "#header")
         self.assert_element_not_visible("#message")
-
     
     # T1-P13, T3-R5-P3
     def test_high_price(self, *_):
-        self.create_user_and_login("Example10", "example10@gmail.com", "Examp!3")
+        self.create_user_and_login("Example10", "example10@gmail.com",
+                                   "Examp!3")
 
         # open create_listing page
         self.open(base_url + '/create_listing')
@@ -328,10 +325,10 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_text("Create a New Listing!", "#header")
         self.assert_element_not_visible("#message")
 
-
     # T3-R1-P3   
     def test_prefix_space(self, *_):
-        self.create_user_and_login("Example11", "example11@gmail.com", "Examp!3")
+        self.create_user_and_login("Example11", "example11@gmail.com",
+                                   "Examp!3")
 
         # open create_listing page
         self.open(base_url + '/create_listing')
@@ -349,7 +346,8 @@ class FrontEndCreateListingTest(BaseCase):
 
     # T3-R1-P4   
     def test_suffix_space(self, *_):
-        self.create_user_and_login("Example12", "example12@gmail.com", "Examp!3")
+        self.create_user_and_login("Example12", "example12@gmail.com",
+                                   "Examp!3")
 
         # open create_listing page
         self.open(base_url + '/create_listing')
@@ -365,10 +363,10 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_element("#message")
         self.assert_text("Could not create listing.", "#message")
     
-
     # T3-R8-P1
     def test_create_2_different_listings(self, *_):
-        self.create_user_and_login("Example13", "example13@gmail.com", "Examp!3")
+        self.create_user_and_login("Example13", "example13@gmail.com",
+                                   "Examp!3")
 
         # Create the first listing
 
@@ -401,12 +399,12 @@ class FrontEndCreateListingTest(BaseCase):
         self.assert_element("#welcome-header")
         self.assert_element("#listings")
         self.assert_text("A Title 1")
-        self.assert_text("A Title 2")
-        
+        self.assert_text("A Title 2")        
 
     # T3-R8-P2
     def test_create_2_equal_listings(self, *_):
-        self.create_user_and_login("Example14", "example14@gmail.com", "Examp!3")
+        self.create_user_and_login("Example14", "example14@gmail.com",
+                                   "Examp!3")
 
         # Create the first listing
 
